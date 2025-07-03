fba8fea388dd4436840080296a5719c229ae2ab8
soni-nishad07 <nishadsoni104@gmail.com>
2025-07-03 15:07:15 +0530

    Add files via upload

---
+++ istriwala.sql
@@ -0,0 +1,228 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 03, 2025 at 11:35 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `istriwala`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_users`
+--
+
+CREATE TABLE `admin_users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin_users`
+--
+
+INSERT INTO `admin_users` (`id`, `name`, `email`, `password`) VALUES
+(1, 'Administrators', 'admin@gmail.com', '$2y$10$f39wNZnwltAtnu4yQLnNx.lyLPeL4CHY36pA3P84Je/9KQDC8x3IG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `apartment_names`
+--
+
+CREATE TABLE `apartment_names` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `apartment_names`
+--
+
+INSERT INTO `apartment_names` (`id`, `name`, `created_at`) VALUES
+(1, 'lucknow', '2025-07-03 07:46:19'),
+(2, 'Amausi', '2025-07-03 07:46:32'),
+(3, 'Alambhag', '2025-07-03 07:46:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `area` varchar(100) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `first_name`, `last_name`, `email`, `phone`, `area`, `message`, `created_at`) VALUES
+(1, 'Soni', 'Nishad', 'soni@genxwebhosting.us', '9874563210', 'ikhkh', 'kjgkubjk', '2025-06-25 17:20:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `apartment_name` varchar(100) NOT NULL,
+  `pin_code` varchar(10) NOT NULL,
+  `address` text NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id`, `full_name`, `apartment_name`, `pin_code`, `address`, `email`, `phone`, `password`, `created_at`) VALUES
+(1, 'sn', 'sector', '8520', 'lko', 'sonigenxwebhosting@gmail.com', '8529637411', '$2y$10$hjsrX5taV5cCnlCH5NGP7.Cv29oBCsOFGz0vcwuYUUwhSghkSAfR6', '2025-06-25 04:26:27'),
+(2, 'Sa', 'demo', '226008', 'new lko', 'ss@gmail.com', '8529631470', '$2y$10$uo5Glm8wsGfwJRAbspQk/ujT1pmARua/KTqfxghH5MM5ziyoqaGUq', '2025-06-25 12:41:54'),
+(3, 'Test user', 'delhi', '225032', 'sector 16', 'demo@gmail.com', '2957413801', '$2y$10$BReowg00XfMK.KssnK82gOLa2PRbV0A4T285qeSlvGWXjVRg3N/IW', '2025-06-26 09:53:57'),
+(4, 'A', 'Amausi', '354', 'jljlk', 'test@gmail.com', '9874563210', '$2y$10$.EEBwdUpxhNSuzM6A/OX6ehmco2kfQJkdsjH1EsHSu5H7gBqdsBxK', '2025-07-03 08:22:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `cloth_count` int(11) NOT NULL,
+  `address` text NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `payment_mode` varchar(50) NOT NULL,
+  `order_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `customer_id`, `cloth_count`, `address`, `amount`, `payment_mode`, `order_date`) VALUES
+(1, 1, 10, 'ss', 100.00, 'Cash', '2025-06-25 12:14:46'),
+(2, 2, 3, 'dd', 30.00, 'Cash', '2025-06-25 12:46:20'),
+(3, 2, 2, 'new lko', 20.00, 'Cash', '2025-06-26 09:48:28'),
+(4, 3, 10, 'sector 16', 100.00, 'Cash', '2025-06-26 09:58:09'),
+(5, 4, 2, 'jljlk', 20.00, 'Cash', '2025-07-03 08:22:42');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_users`
+--
+ALTER TABLE `admin_users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `apartment_names`
+--
+ALTER TABLE `apartment_names`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_users`
+--
+ALTER TABLE `admin_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `apartment_names`
+--
+ALTER TABLE `apartment_names`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
